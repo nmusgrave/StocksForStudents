fc4733ff4cfdeb55f5d11fa97adfbf6e91d11ce2
David Coven <mrcoven94@gmail.com>
2015-01-17 14:59:08 -0500

    add new dir structure for express routing, including views

--- server/bloom.js
+++ server/bloom.js
@@ -7,10 +7,11 @@ var app = express()
 var server = require('http').createServer(app)
 
 server.listen(3000, function() {
-    console.log('server running')
+    console.log('server running on http://localhost:3000/')
 })
 
 app.get('/', function(req, res) {
+app.use(express.static(__dirname + '/public'));
 
     bloomberg(function(data) {
         console.log(data.data[0].securityData)
@@ -25,4 +26,6 @@ app.get('/', function(req, res) {
         res.send(data)
     })
 })
+
+
 
