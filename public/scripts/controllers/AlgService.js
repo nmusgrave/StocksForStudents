a91969160b7b7e8d2d7d143886e4528971dcfb05
nmusgrave <naomi.g.musgrave@gmail.com>
2015-01-17 18:28:24 -0800

    working on algs

--- public/scripts/controllers/AlgService.js
+++ public/scripts/controllers/AlgService.js
@@ -1,6 +1,87 @@
 seedApp.service('algorithm', function() {
-  this.test = function() {
-    console.log('foo')
-  } 
-   
+
+  // orders
+  this.newOrder = function(sec, order, bs) {
+    return {
+      security: sec,
+      orderPrice: order,
+      buySell: bs
+    }
+  }
+
+  // markets 
+  this.newMarket = function(sec, bid, ask) {
+    return {
+      security : sec,
+      //holds highest buy price in buy order book
+      bidQuote : bid,
+      //holds lowest sell price in sell order book
+      askQuote : ask,
+      //buy order book
+      buyOrders : [],
+      //sell order book
+      sellOrders : []
+    }
+  }
+  this.addOrder = function(market, order) {
+    for(var i = 0; i < order.length; i++) {
+      if(order[i].buySell >= 1)
+        market.buyOrders.push(order[i])
+      else
+        market.sellOrders.push(order[i])
+    }
+    console.log(market)
+    return market
+  }
+  // input: market obj, int
+  this.computePercentageChange = function (market, buyOrSell) {
+    console.log(market)
+    if(buyOrSell >= 1) {
+      console.log(market.buyOrders[0].orderPrice)
+      return (market.buyOrders[market.buyOrders.length - 1].orderPrice - 
+        market.buyOrders[0].orderPrice) / 100;
+    } else {
+      console.log('BAD')
+      return (market.sellOrders[market.sellOrders.length - 1].orderPrice - market.sellOrders[0].orderPrice) / 100;
+    }
+  }
+  // input: int 
+  this.computeAveragePrice = function(market, buyOrSell) {
+    var sum = 0
+    if(buyOrSell >= 1) {
+      for(var i = 0; i < market.buyOrders.length; i++) 
+        sum += market.buyOrders[i].orderPrice
+      console.log(sum)
+      return sum / market.buyOrders.length
+    } else {
+      for(var j = 0; j < market.sellOrders.length; j++) 
+        sum += market.sellOrders[i].orderPrice
+      console.log(sum)
+      return sum / market.sellOrders.length
+    }
+  }
+  this.getBidQuote = function(market) {
+    var max = 0.0
+    for(var i = 0; i < market.buyOrders.length; i++)
+      if(market.buyOrders[i].orderPrice > max)
+        max = market.buyOrders[i].orderPrice
+    return max
+  }
+  this.getAskQuote = function(market) {
+    var min = 0.0
+    for(var i = 0; i < market.sellOrders.length; i++)
+      if(market.sellOrders[i].orderPrice < min)
+        min = market.sellOrders[i].orderPrice
+    return min
+  }
+
+
+  //investor
+  this.newInvestor = function() {
+    return {
+      cashBalance: 0.0,
+      orders: []
+    }
+  }
+ 
  })
--- public/scripts/controllers/MainCtrl.js
+++ public/scripts/controllers/MainCtrl.js
@@ -1,10 +1,8 @@
 // create the controller and inject Angular's $scope
-seedApp.controller('mainController', function($scope, $log, algorithm) {
+seedApp.controller('mainController', function($scope, $log, $q, algorithm) {
 	// create a message to display in our view
 	$scope.message = 'Everyone come and see how FREAKING AWESOME I look!';
 
-  algorithm.test();
-	
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
   $scope.data = [
@@ -12,5 +10,31 @@ seedApp.controller('mainController', function($scope, $log, algorithm) {
     [28, 48, 40, 19, 86, 27, 90]
   ];
 
+
+  // testing algs
+  $q.all([algorithm.newOrder("abc", 100, 1),
+    algorithm.newOrder("abc", 105, 1),
+    algorithm.newOrder("abc", 110, 1),
+    algorithm.newOrder("abc", 107, 1),
+    algorithm.newOrder("abc", 108, 1),
+    algorithm.newOrder("abc", 105, 1), 
+    algorithm.newMarket("abc")]).then(function(result) {
+      console.log(result);
+      console.log('CALLBACK')
+      $q.all([
+        algorithm.addOrder(result[6], 
+          [result[0], result[1], result[2], result[3],
+          result[4], result[5] ]) ])
+      .then(function(result2) {
+        console.log(result2)
+        console.log('inner callback')
+        $q.all([algorithm.computePercentageChange(result2[0], 2), 
+          algorithm.computeAveragePrice(result2[0], 1),
+          algorithm.computeAveragePrice(result2[0], -1)])
+        .then(function(result3) {
+          console.log(result3)
+        })
+      })
+    })
 
 });
