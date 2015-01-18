a2eddc84c98d8582ba55336f0c68174e0d3d0965
David Coven <mrcoven94@gmail.com>
2015-01-17 19:05:53 -0500

    add design to root page

--- public/scripts/controllers/MainCtrl.js
+++ public/scripts/controllers/MainCtrl.js
@@ -1,13 +1,13 @@
 // create the controller and inject Angular's $scope
 seedApp.controller('mainController', function($scope, $log, algorithm) {
 	// create a message to display in our view
-	$scope.message = 'Everyone come and see how FREAKING AWESOME I look!';
+	$scope.message = 'Trading stocks in as little as three clicks.';
 
-  algorithm.test();
+    algorithm.test();
 	
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
     [28, 48, 40, 19, 86, 27, 90]
   ];
